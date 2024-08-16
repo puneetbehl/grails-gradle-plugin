8cdbb11f93ca84f3186990ae541028a4b9e1054d
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-08-15 20:56:38 -0600

    Fixes #314

--- src/main/groovy/org/grails/gradle/plugin/web/gsp/GspCompileOptions.groovy
+++ src/main/groovy/org/grails/gradle/plugin/web/gsp/GspCompileOptions.groovy
@@ -4,26 +4,25 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.compile.AbstractOptions
-import org.gradle.api.tasks.compile.BaseForkOptions
 import org.gradle.api.tasks.compile.GroovyForkOptions
 import javax.inject.Inject;
 
 /**
-* Presents the Compile Options used by the {@llink GroovyPageForkCompileTask}
-*
-* @author David Estes
-* @since 4.0
-*/
-class GspCompileOptions extends AbstractOptions {
+ * Presents the Compile Options used by the {@llink GroovyPageForkCompileTask}
+ *
+ * @author David Estes
+ * @since 4.0
+ */
+abstract class GspCompileOptions extends AbstractOptions {
     private static final long serialVersionUID = 0L;
 
     @Input
-		String encoding = "UTF-8"
+    String encoding = "UTF-8"
 
-		@Inject
-		protected ObjectFactory getObjectFactory() {
-			throw new UnsupportedOperationException();
-		}
+    @Inject
+    protected ObjectFactory getObjectFactory() {
+        throw new UnsupportedOperationException();
+    }
 
     @Nested
     GroovyForkOptions forkOptions = getObjectFactory().newInstance(GroovyForkOptions.class)
