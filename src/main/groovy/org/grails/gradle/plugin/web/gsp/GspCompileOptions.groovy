8f926fe7c04f4726b2006c2dff74efde84667ac0
James Daugherty <jdaugherty@jdresources.net>
2024-12-03 10:46:40 -0500

    Updating copyright

--- src/main/groovy/org/grails/gradle/plugin/web/gsp/GspCompileOptions.groovy
+++ src/main/groovy/org/grails/gradle/plugin/web/gsp/GspCompileOptions.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.grails.gradle.plugin.web.gsp
 
 import org.gradle.api.model.ObjectFactory
@@ -5,7 +20,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.compile.AbstractOptions
 import org.gradle.api.tasks.compile.GroovyForkOptions
-import javax.inject.Inject;
+import javax.inject.Inject
 
 /**
  * Presents the Compile Options used by the {@llink GroovyPageForkCompileTask}
