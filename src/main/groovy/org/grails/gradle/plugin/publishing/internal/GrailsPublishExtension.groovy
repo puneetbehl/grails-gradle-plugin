bad6cd2d871f2d19611e25ac37276d26b4062532
Mattias Reichel <mattias.reichel@gmail.com>
2024-12-04 09:34:41 +0100

    fix: replace usages of deprecated `ConfigureUtil` (#359)
    The `org.gradle.util.ConfigureUtil` type has been deprecated.
    This is scheduled to be removed in Gradle 9.0.

--- src/main/groovy/org/grails/gradle/plugin/publishing/internal/GrailsPublishExtension.groovy
+++ src/main/groovy/org/grails/gradle/plugin/publishing/internal/GrailsPublishExtension.groovy
@@ -17,7 +17,6 @@
 package org.grails.gradle.plugin.publishing.internal
 
 import groovy.transform.CompileStatic
-import org.gradle.util.ConfigureUtil
 
 /**
  * @author Puneet Behl
@@ -124,7 +123,10 @@ class GrailsPublishExtension {
      * @return the license instance
      */
     License license(@DelegatesTo(License) Closure configurer) {
-        ConfigureUtil.configure(configurer, license)
+        configurer.delegate = license
+        configurer.resolveStrategy = Closure.DELEGATE_FIRST
+        configurer.call()
+        return license
     }
 
     void setLicense(License license) {
